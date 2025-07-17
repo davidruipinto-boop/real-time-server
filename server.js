c3e85c929ef718522d73850feb4dcd54ff4fcf0a
davidruipinto-boop <david.rui.pinto@gmail.com>
2025-07-17 10:00:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,50 @@
-const PORT = process.env.PORT || 3000;
-const io = require('socket.io')(3000, {
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
   cors: {
     origin: "https://davidruipinto-boop.github.io",
     methods: ["GET", "POST"]
   }
 });
 
-const users = {}
+const PORT = process.env.PORT || 3000;
+
+const users = {};
 
 io.on('connection', socket => {
-    socket.on('new-user', name => {
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
-    })
-    socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-    })
+  socket.on('new-user', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+  });
 
-    socket.on('send-file', data => {
-        socket.broadcast.emit('file-message', data);
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', {
+      message: message,
+      name: users[socket.id]
     });
+  });
 
+  socket.on('send-file', data => {
+    socket.broadcast.emit('file-message', data);
+  });
 
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconected', users[socket.id])
-        delete users[socket.id]
-    })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconected', users[socket.id]);
+    delete users[socket.id];
+  });
+});
 
-})
+// Endpoint de teste opcional (só para verificar se o servidor responde)
+app.get('/', (req, res) => {
+  res.send("Servidor Socket.IO está ativo");
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor a correr na porta ${PORT}`);
+});
